11569ba92f9fe4d0373e2752abd8e87824b5d799
Dustin McCormick <dmccormick@torchlite.com>
2016-11-04 23:33:07 -0400

    Use new ES6 language features. Make code more succinct.

--- index.js
+++ index.js
@@ -1,38 +1,83 @@
-var Contacts = require('./resources/contacts.js');
-var Journeys = require('./resources/journeys.js');
-var Lists = require('./resources/lists.js');
-var Account = require('./resources/account.js');
+let axios = require('axios');
 
-function Autopilot(apiKey) {
-	this.options = {
-		api: {
-			base: 'https://api2.autopilothq.com/v1',
-			key: apiKey,
+let { Contacts, Journeys, Lists, Account } = require('./resources');
+
+module.exports = class Autopilot {
+	constructor(apiKey) {
+		this.api = axios.create({
+			baseURL: 'https://api2.autopilothq.com/v1/',
 			headers: {
-				'autopilotapikey': apiKey,
-				'Accept': 'application/json'
+				autopilotapikey: apiKey,
+				Accept: 'application/json'
 			}
+		});
+	}
+
+	account = {
+		get: (callback) => {
+			return this.api.delete(`account`)
+				.then(callback || res => res);
+		}
+	},
+
+	contacts = {
+		upsert: (data, callback) => {
+			let key = Array.isArray(data) ? 'contacts' : 'contact';
+
+			return this.api.post(key, { [key]: data })
+				.then(callback || res => res);
+		},
+		get: (contactId, callback) => {
+			return this.api.get(`contact/${contactId}`)
+				.then(callback || res => res);
+		},
+		delete: (contactId, callback) => {
+			return this.api.delete(`contact/${contactId}`)
+				.then(callback || res => res);
+		},
+		unsubscribe: (contactId, callback) => {
+			return this.api.post(`contact/${contactId}/unsubscribe`)
+				.then(callback || res => res);
 		}
 	};
 
-	this.account = new Account(this);
-	this.contacts = new Contacts(this);
-	this.lists = new Lists(this);
-	this.journeys = new Journeys(this);
+	journeys = {
+		list: (callback) => {
+			return this.api.get('triggers')
+				.then(callback || res => res);
+		},
+		add: (triggerId, contactId, callback) => {
+			return this.api.post(`trigger/${triggerId}/contact/${contactId}`)
+				.then(callback || res => res);
+		}
+	};
+
+	lists = {
+		list: (callback) => {
+			return this.api.get('lists')
+				.then(callback || res => res);
+		},
+		insert: (name, callback) => {
+			return this.api.post('list', { name })
+				.then(callback || res => res);
+		},
+		roster: (listId, bookmark, callback) => {
+			bookmark = bookmark ? `/${bookmark}` : '';
+
+			return this.api.get(`list/${listId}/contacts${bookmark}`)
+				.then(callback || res => res);
+		},
+		has: (listId, contactId, callback) => {
+			return this.api.get(`list/${listId}/contact/${contactId}`)
+				.then(callback || res => res);
+		},
+		add: (listId, contactId, callback) => {
+			return this.api.post(`list/${listId}/contact/${contactId}`)
+				.then(callback || res => res);
+		},
+		remove: (listId, contactId, callback) => {
+			return this.api.delete(`list/${listId}/contact/${contactId}`)
+				.then(callback || res => res);
+		}
+	};
 }
-
-Autopilot.prototype.result = function (promise, callback) {
-	if (callback) {
-		promise
-			.then(function (res) {
-				callback(null, res);
-			})
-			.catch(function (res) {
-				callback(new Error(), res);
-			});
-	}
-
-	return promise;
-}
-
-module.exports = Autopilot;
